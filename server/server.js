12b80cfd8b0c70f0d89492198a627165c3768ed6
rathinakumar-dev <rathinakumar825@gmail.com>
2025-08-05 18:43:15 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import userRouter from './routes/UserRoutes.js'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import userRouter from "./routes/UserRoutes.js";
 // App Config
-const  PORT = process.env.PORT || 4000;
-const app = express(); 
+const PORT = process.env.PORT || 4000;
+const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // API Routes
-app.get('/', (req, res) => res.send('API is working'));
-app.use('/api/user', userRouter);
-
+app.get("/", (req, res) => res.send("API is working"));
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => console.log("Server is running on port " + PORT));
